2082b46892b02dc5f2223ee59fadd2fb4b33b6d0
dantr <huynhngdantr@gmail.com>
2023-12-05 15:26:26 +0700

    navbar for page

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Navbar() {
+  return (
+    <nav className="bg-gray-800">
+      <div className="h-16 flex items-center px-8">
+        <p className="text-white font-bold capitalize">
+          Employeer management system
+        </p>
+      </div>
+    </nav>
+  );
+}
